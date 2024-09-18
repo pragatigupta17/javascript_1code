33b0b4fa810029d6b63a810080ccd5ab47d0b357
Pragati Gupta <pragatigupta0104@gmail.com>
2024-09-18 12:23:41 +0530

    thirddaycommit

---
+++ class3.js
@@ -0,0 +1,22 @@
+//function class3(){
+   // let a =parseInt(window.prompt("enter number 1"));
+  // let b = parseInt(window.prompt("enter number 2"));
+  //  window.alert(a+b);
+  //  console.log(a+b);
+
+//}
+//class3();
+//ECMA Script6 is the real name oF javascript
+//ECMA is organization which is combination of two companies name Netscape , Microsoft (internet explorer)
+function class3(){
+    var a = "p35 js";
+    console.log(a);
+     var a = 90;
+     console.log(a);
+     let b = "p35js";
+     b = 80;
+     console.log(b);
+     const c = "p35js";
+     console.log(c);
+}
+class3();
