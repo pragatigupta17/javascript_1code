14429b3b4392f8a8c65ce8c415db7440bf7e77f5
Pragati Gupta <pragatigupta0104@gmail.com>
2024-09-23 12:23:26 +0530

    datatype_commit

---
+++ datatype.js
@@ -0,0 +1,16 @@
+let sum=null;
+console.log(typeof(sum));
+
+sum=90;
+console.log(typeof(sum));
+
+sum="dfbyjhy";
+console.log(typeof(sum));
+
+
+//BIGINT DATATYPE
+
+let num=1234567789023456789065n;
+let num1=678964n;
+console.log(num + num1);
+console.log(typeof(num));
