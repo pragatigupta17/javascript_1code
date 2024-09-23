18803f23f96c8243627355c21cbcf469411450b9
Pragati Gupta <pragatigupta0104@gmail.com>
2024-09-23 12:31:21 +0530

    commit

--- datatype.js
+++ datatype.js
@@ -14,3 +14,15 @@ let num=1234567789023456789065n;
 let num1=678964n;
 console.log(num + num1);
 console.log(typeof(num));
+
+let a="pragati";
+let b="pragati";
+console.log(a===b);
+
+let A = symbol("PRAGATI");
+let B =symbol("PRAGATI");
+console.log(A===B);
+
+let c='45'
+let d= 45;
+console.log(c===d);
