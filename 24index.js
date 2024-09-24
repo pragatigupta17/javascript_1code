140a35a5fbafc099e994fba1a94d9e2f7b448f44
Pragati Gupta <pragatigupta0104@gmail.com>
2024-09-24 12:25:02 +0530

    change_image

---
+++ 24index.js
@@ -0,0 +1,4 @@
+function change(){
+    let selecteddiv = document.querySelector('#img1')
+    selecteddiv.style.backgroundImage = "url('https://images.designtrends.com/wp-content/uploads/2016/04/02133843/Beautiful-Horizon-Background.jpg')";
+}
