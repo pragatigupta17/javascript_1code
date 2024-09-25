12177113dc3398cddd8c497f61916e3f871f7755
Pragati Gupta <pragatigupta0104@gmail.com>
2024-09-25 12:33:35 +0530

    remark

---
+++ attribute.js
@@ -0,0 +1,9 @@
+function  run(){
+    let selectphoto = document.querySelector('#photo');
+    selectphoto.src="https://tse2.mm.bing.net/th?id=OIP.fVT57bEaGCWA3V7a3DnnsQHaEo&pid=Api&P=0&h=180"
+}
+// 
+function run(){
+    let selectedul = document.querySelector('ul');
+    selectedul.type= 'circle'
+}
