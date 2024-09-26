7812ba99b4327e33de0f59b04a05a61158072072
Pragati Gupta <pragatigupta0104@gmail.com>
2024-09-26 18:36:02 +0530

    pics_change_commit

--- attribute.js
+++ attribute.js
@@ -1,9 +1,29 @@
-function  run(){
-    let selectphoto = document.querySelector('#photo');
-    selectphoto.src="https://tse2.mm.bing.net/th?id=OIP.fVT57bEaGCWA3V7a3DnnsQHaEo&pid=Api&P=0&h=180"
-}
+// function  run(){
+    // let selectphoto = document.querySelector('#photo');
+    // selectphoto.src="https://tse2.mm.bing.net/th?id=OIP.fVT57bEaGCWA3V7a3DnnsQHaEo&pid=Api&P=0&h=180"
+// }
 // 
-function run(){
-    let selectedul = document.querySelector('ul');
-    selectedul.type= 'circle'
+//function run(){
+  //  let selectedul = document.querySelector('ul');
+   // selectedul.type= 'circle'
+//}
+////
+function change1(){
+    let selectedlargeimg = document.querySelector('#largeimg');
+    selectedlargeimg.src = "https://m.media-amazon.com/images/I/61UzQMCRaeL._SX522_.jpg";
+}
+
+function change2(){
+    let selectedlargeimg = document.querySelector('#largeimg');
+    selectedlargeimg.src = "https://m.media-amazon.com/images/I/71UEAZPW7+L._SX522_.jpg";
+}
+
+function change3(){
+    let selectedlargeimg = document.querySelector('#largeimg');
+    selectedlargeimg.src = "https://m.media-amazon.com/images/I/71sYkZraPrL._SX522_.jpg";
+}
+
+function change4(){
+    let selectedlargeimg = document.querySelector('#largeimg');
+    selectedlargeimg.src = "https://m.media-amazon.com/images/I/7166Kutg5fL._SX522_.jpg";
 }
