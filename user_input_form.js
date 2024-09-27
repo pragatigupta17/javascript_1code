6699a0e1522456b1e3a832a13a034379ccbf004a
Pragati Gupta <pragatigupta0104@gmail.com>
2024-09-27 12:31:58 +0530

    form

--- user_input_form.js
+++ user_input_form.js
@@ -3,17 +3,59 @@ function data(){
     let age = document.querySelector('#age').value;
     let mail = document.querySelector('#mail').value;
     let address = document.querySelector('#address').value;
-    let password = document.querySelector('#password').value;
+    let no = document.querySelector('#no').value;
     let cpswd = document.querySelector('#cpswd').value;
     console.log(name);
     console.log(age);
     console.log(mail);
     console.log(address);
-    console.log(password);
+    console.log(no);
     console.log(cpswd);
     if(name === ""){
-        window.alert("please enter ur name");
+       // window.alert("please enter ur name");
+       document.querySelector('#errorname').innerHTML = "please enter ur name";
+       let selectname = document.querySelector('#name');
+       selectname.style.borderColor = "red"
+       selectname.style.outlineColor = "red"
+       return false;
+    }
+    if(age === ""){
+        document.querySelector('#errorage').innerHTML = "please enter ur age";
+        let selectage = document.querySelector('#age');
+        selectage.style.borderColor = "red"
+        selectage.style.borderColor = "red"
         return false;
     }
 
+    if(mail === ""){
+        document.querySelector('#errormail').innerHTML = "please enter ur mail";
+        let selectage = document.querySelector('#mail');
+        selectage.style.borderColor = "red"
+        selectage.style.borderColor = "red"
+        return false;
+    }
+
+    if(address === ""){
+        document.querySelector('#erroraddress').innerHTML = "please enter ur address";
+        let selectage = document.querySelector('#address');
+        selectage.style.borderColor = "red"
+        selectage.style.borderColor = "red"
+        return false;
+    }
+
+    if(no === ""){
+        document.querySelector('#errorno').innerHTML = "please enter ur no";
+        let selectage = document.querySelector('#no');
+        selectage.style.borderColor = "red"
+        selectage.style.borderColor = "red"
+        return false;
+    }
+
+    if(cpswd === ""){
+        document.querySelector('#errorcpswd').innerHTML = "please enter ur cpswd";
+        let selectage = document.querySelector('#cpswd');
+        selectage.style.borderColor = "red"
+        selectage.style.borderColor = "red"
+        return false;
+    }
 }
