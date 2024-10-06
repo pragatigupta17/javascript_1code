df21da1b7d1f159b726619397dfa1e6b31403bf6
Pragati Gupta <pragatigupta0104@gmail.com>
2024-10-06 19:20:12 +0530

    template

--- template.js
+++ template.js
@@ -1,31 +1,25 @@
 function card(){
-    let name = document.querySelector('#name').value;
-    let phone = document.querySelector('#phone').value;
-    let email = document.querySelector('#email').value;
-    let designation = document.querySelector('#designation').value;
-    let address = document.querySelector('#address').value;
-    let image = document.querySelector('#image').value;
-
-    console.log(name,phone,email,designation,address,image);
-
-
-    let d = ` <section id="card">
-        <div>
-            <img src="${image}" alt="">
-        </div>
-        <div>
-            <h1>${name}</h1>
-            <h3>${phone}</h3>
-            <h3>${email}</h3>
-            <h3>${designation}</h3>
-            <h3>${address}</h3>
-        </div>
-    </section>`;
-
-    alert("write code");
-
-    document.querySelector('#output').innerHTML = d;
-    document.querySelector('form').style.display = "none"
-    print()
-    return false;
+    let name=document.querySelector('#name').value;
+    let Phone=document.querySelector('#Phone').value;
+    let email=document.querySelector('#email').value;
+    let designation=document.querySelector('#designation').value;
+    let address=document.querySelector('#address').value;
+    let image=document.querySelector('#img').value;
+    console.log(name,Phone,email,designation,address,img)
+    let inputvalue= `   <section id="card">
+        <div id="detail">
+    <h1> ${name}</h1>
+    <h3>${Phone}</h3>
+    <h3>${email}</h3>
+    <h3>${designation}</h3>
+    <h3>${address}</h3>
+    </div>
+    <div id="image">
+        <img src="${image}" alt="img not found">
+    </div>
+    </section>`
+    let output=document.querySelector('#output').innerHTML=inputvalue;
+    document.querySelector('form').style.display="none"
+    print();
+    
 }
