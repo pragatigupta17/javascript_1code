4dcde1514f31519fc158eae7588e3bb298fd08e7
Pragati Gupta <pragatigupta0104@gmail.com>
2024-10-10 12:15:23 +0530

    string

---
+++ string_method.js
@@ -0,0 +1,15 @@
+let str = "this is js class";
+console.log(str);
+console.log(str[0]);
+
+//// -------> METHOD OF STRING <--------///////
+console.log(str.length);
+console.log(str.replace("",""));
+console.log(str.replaceAll("",""));
+console.log(str.includes("sj"));
+console.log(str.slice(3,6))
+console.log(str.slice(11,str.length))
+console.log(str.trim());
+
+
+
