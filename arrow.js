abb43676b1a6c1a82ccee613a3d25fde056281b4
Pragati Gupta <pragatigupta0104@gmail.com>
2024-11-12 14:26:24 +0530

    arrow_function

---
+++ arrow.js
@@ -0,0 +1,11 @@
+//  1st syntax ---- 
+let b=() => {return "js"}
+console.log(b())
+let z=(a,b) => {return a+b}
+console.log(z(2,3))
+// 2nd syntax ---- ()=>()
+    // 3rd syntax --- ()=>
+        // 4th syntax ---   =>
+// wap to find out area of circle by using arrow function 
+let area = (r) =>{return 3.14*r*r}
+console.log(area(20))
