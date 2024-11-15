d4c5d3378a50d670f388ae57a8bc72af72d5229f
Pragati Gupta <pragatigupta0104@gmail.com>
2024-11-15 14:33:12 +0530

    form

--- form_local.js
+++ form_local.js
@@ -9,10 +9,19 @@ function save(){
     }
 
    localStorage.setItem("data",JSON.stringify(ob))
-
-
-
-}
+   }
 
    let m=JSON.parse(localStorage.getItem("data"))
    console.log(m);
+ let myname=document.querySelector("#myname")
+ let myage=document.querySelector("#myage")
+ let mymail=document.querySelector("#mymail")
+ let myaddress=document.querySelector("#myaddress")
+let myno=document.querySelector("#myno")
+let mycpswd=document.querySelector("#mycpswd")
+myname.innerHTML=m.name
+myage.innerHTML=m.age
+mymail.innerHTML=m.mail
+myaddress.innerHTML=m.address
+myno.innerHTML=m.no
+mycpswd.innerHTML=m.cpswd
